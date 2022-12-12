8d0f53491227cb9ee33806437064db89fd1e036d
YongJoon <45617099+nepanepa@users.noreply.github.com>
2022-12-13 01:43:36 +0900

    Add files via upload

---
+++ TEST3-1/2048.js
@@ -0,0 +1,199 @@
+var board; //보드 만들고
+var score = 0; // 점수 변수
+var rows = 4;
+var columns = 4;
+
+window.onload = function() { //window.onload 사용한다
+    //페이지가 로드되면 자동으로 실행되는 전역 콜백함수
+    setGame(); //함수 선언
+}
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0], //보드 만들기
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    // board = [
+    //     [2, 2, 2, 2], //보드 만들기
+    //     [2, 2, 2, 2],
+    //     [4, 4, 4, 4],
+    //     [4, 4, 4, 4]
+    // ]
+
+    for (let r = 0; r < rows; r++) { //for문 사용
+        for (let c = 0; c < columns; c++) {
+            // <div id = "0-0"></div>
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c]; //num과 rc 보드를 같게 하고
+            //스타일을 적용하는 업데이트 타일 함수 호출 
+            updateTile(tile, num);
+            document.getElementById("board").append(tile); //div태그를 보드에 추가
+        }
+    }
+
+    setTwo(); //함수 호출
+    setTwo(); //함수 호출
+}
+
+function hasEmptyTile() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            if (board[r][c] == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function setTwo() {
+    if (!hasEmptyTile()) { //빈 타일이 없으면 bool를 반환
+        return;
+    }
+
+    let found = false;
+    while (!found) {
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c,toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+        }
+    }
+}
+
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; //class 생성
+    tile.classList.add("tile"); //tile를 다시 추가하여 tile class 목록 tile를 만듦
+    if (num > 0) { //num이 0보다 크면 배수가 됨
+        tile.innerText = num;
+        if (num <= 4096) {
+            tile.classList.add("x"+num.toString()); 
+        }else {
+            tile.classList.add("x8192");
+        }
+    }
+}  
+
+document.addEventListener("keyup", (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+        setTwo(); //슬라이드할 때마다 2개의 타일 생성
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+        setTwo(); //슬라이드할 때마다 2개의 타일 생성
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+        setTwo(); //슬라이드할 때마다 2개의 타일 생성
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+        setTwo(); //슬라이드할 때마다 2개의 타일 생성
+    }
+    document.getElementById("score").innerText = score; //점수
+})
+
+function filterZero(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    //[0, 2, 2, 2]
+    row = filterZero(row);
+
+    //slide
+    for (let i = 0; i < row.length-1; i++) {
+        if (row[i] == row[i+1]) {
+            row[i] *= 2;
+            row[i+1] = 0;
+            score += row[i];
+        }
+    }
+
+    row = filterZero(row);
+
+    while(row.length < columns) {
+        row.push(0);
+    }
+
+    return row;
+}
+
+function slideLeft() { //보드의 각 행을 반복
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row); //함수 호출
+        board[r] = row;
+
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideRight() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row.reverse(); //역순
+        row = slide(row);
+        row.reverse(); //역순
+        board[r] = row;
+
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num); 
+        }
+    }
+}
+
+function slideUp() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = slide(row);
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
+
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row.reverse(); //역순
+        row = slide(row);
+        row.reverse(); //역순
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
+
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
